770f87ed96eb4f6157c24e87631ee482cf288948
baguchan <shutux500@gmail.com>
2024-11-30 10:00:59 +0900

    backport mini crossbow

--- src/main/java/baguchan/hunters_return/client/HunterRenderingRegistry.java
+++ src/main/java/baguchan/hunters_return/client/HunterRenderingRegistry.java
@@ -5,11 +5,20 @@ import baguchan.hunters_return.client.model.OldHunterModel;
 import baguchan.hunters_return.client.render.BoomerangRender;
 import baguchan.hunters_return.client.render.HunterRender;
 import baguchan.hunters_return.init.HunterEntityRegistry;
+import baguchan.hunters_return.init.HunterItems;
+import baguchan.hunters_return.item.MiniCrossBowItem;
+import net.minecraft.client.renderer.item.ItemProperties;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CrossbowItem;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.component.ChargedProjectiles;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.api.distmarker.OnlyIn;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
+import net.neoforged.neoforge.client.event.ModelEvent;
 
 @OnlyIn(Dist.CLIENT)
 @EventBusSubscriber(modid = baguchan.hunters_return.HuntersReturn.MODID, value = Dist.CLIENT, bus = EventBusSubscriber.Bus.MOD)
@@ -24,5 +33,42 @@ public class HunterRenderingRegistry {
     public static void registerLayerDefinition(EntityRenderersEvent.RegisterLayerDefinitions event) {
         event.registerLayerDefinition(ModModelLayers.HUNTER, HunterModel::createBodyLayer);
         event.registerLayerDefinition(ModModelLayers.HUNTER_OLD, OldHunterModel::createBodyLayer);
+    }
+
+    @SubscribeEvent
+    public static void registerItemModel(ModelEvent.ModifyBakingResult event) {
+        ItemProperties.register(
+                HunterItems.MINI_CROSSBOW.asItem(),
+                ResourceLocation.withDefaultNamespace("pull"),
+                (p_351682_, p_351683_, p_351684_, p_351685_) -> {
+                    if (p_351684_ == null) {
+                        return 0.0F;
+                    } else {
+                        return CrossbowItem.isCharged(p_351682_)
+                                ? 0.0F
+                                : (float) (p_351682_.getUseDuration(p_351684_) - p_351684_.getUseItemRemainingTicks())
+                                / (float) MiniCrossBowItem.getChargeDuration(p_351682_, p_351684_);
+                    }
+                }
+        );
+        ItemProperties.register(
+                HunterItems.MINI_CROSSBOW.asItem(),
+                ResourceLocation.withDefaultNamespace("pulling"),
+                (p_174605_, p_174606_, p_174607_, p_174608_) -> p_174607_ != null
+                        && p_174607_.isUsingItem()
+                        && p_174607_.getUseItem() == p_174605_
+                        && !CrossbowItem.isCharged(p_174605_)
+                        ? 1.0F
+                        : 0.0F
+        );
+        ItemProperties.register(
+                HunterItems.MINI_CROSSBOW.asItem(),
+                ResourceLocation.withDefaultNamespace("charged"),
+                (p_275891_, p_275892_, p_275893_, p_275894_) -> CrossbowItem.isCharged(p_275891_) ? 1.0F : 0.0F
+        );
+        ItemProperties.register(HunterItems.MINI_CROSSBOW.asItem(), ResourceLocation.withDefaultNamespace("firework"), (p_329796_, p_329797_, p_329798_, p_329799_) -> {
+            ChargedProjectiles chargedprojectiles = p_329796_.get(DataComponents.CHARGED_PROJECTILES);
+            return chargedprojectiles != null && chargedprojectiles.contains(Items.FIREWORK_ROCKET) ? 1.0F : 0.0F;
+        });
     }
 }
--- src/main/java/baguchan/hunters_return/init/HunterItems.java
+++ src/main/java/baguchan/hunters_return/init/HunterItems.java
@@ -1,17 +1,19 @@
 package baguchan.hunters_return.init;
 
 import baguchan.hunters_return.item.BoomerangItem;
-import net.minecraft.core.registries.BuiltInRegistries;
+import baguchan.hunters_return.item.MiniCrossBowItem;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.component.ChargedProjectiles;
 import net.neoforged.neoforge.common.DeferredSpawnEggItem;
+import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
-import java.util.function.Supplier;
-
 public class HunterItems {
-	public static final DeferredRegister<Item> ITEM_REGISTRY = DeferredRegister.create(BuiltInRegistries.ITEM, baguchan.hunters_return.HuntersReturn.MODID);
+	public static final DeferredRegister.Items ITEM_REGISTRY = DeferredRegister.createItems(baguchan.hunters_return.HuntersReturn.MODID);
 
-	public static final Supplier<DeferredSpawnEggItem> SPAWNEGG_HUNTER = ITEM_REGISTRY.register("spawnegg_hunter", () -> new DeferredSpawnEggItem(HunterEntityRegistry.HUNTERILLAGER, 9804699, 5777447, (new Item.Properties())));
-	public static final Supplier<Item> BOOMERANG = ITEM_REGISTRY.register("boomerang", () -> new BoomerangItem((new Item.Properties()).durability(384)));
+	public static final DeferredItem<DeferredSpawnEggItem> SPAWNEGG_HUNTER = ITEM_REGISTRY.register("spawnegg_hunter", () -> new DeferredSpawnEggItem(HunterEntityRegistry.HUNTERILLAGER, 9804699, 5777447, (new Item.Properties())));
+	public static final DeferredItem<Item> BOOMERANG = ITEM_REGISTRY.register("boomerang", () -> new BoomerangItem((new Item.Properties()).durability(384)));
+	public static final DeferredItem<Item> MINI_CROSSBOW = ITEM_REGISTRY.register("mini_crossbow", () -> new MiniCrossBowItem((new Item.Properties().durability(412).component(DataComponents.CHARGED_PROJECTILES, ChargedProjectiles.EMPTY).stacksTo(1))));
 
 }
