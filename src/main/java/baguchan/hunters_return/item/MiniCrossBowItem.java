29e9f6d7e736a8224d38da2155a861fdf1cd36c4
baguchan <shutux500@gmail.com>
2024-12-02 17:42:31 +0900

    backport mini crossbow part 2

--- src/main/java/baguchan/hunters_return/item/MiniCrossBowItem.java
+++ src/main/java/baguchan/hunters_return/item/MiniCrossBowItem.java
@@ -125,7 +125,7 @@ public class MiniCrossBowItem extends CrossbowItem {
     }
 
     private static float getShootingPower(ItemStack p_40946_) {
-        return containsChargedProjectile(p_40946_, Items.FIREWORK_ROCKET) ? 1.6F * 0.85F : 2.95F;
+        return containsChargedProjectile(p_40946_, Items.FIREWORK_ROCKET) ? 1.6F * 0.85F : 1.75F;
     }
 
     @Override
